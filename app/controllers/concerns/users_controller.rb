bf9723791de1049d1007f1e6371c5435beabe5c9
Huy Huynh <huyhuynh408@gmail.com>
2015-05-20 16:17:32 -0500

    user create finished

--- app/controllers/concerns/users_controller.rb
+++ app/controllers/concerns/users_controller.rb
@@ -6,8 +6,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to user_path(@user)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render(:new)
+    end
   end
 
   def show
