f175fd5abd54ce9955e5fc26afcff5b49d6f6a7c
cjhaddad & Logan Price <pair+cjhaddad.logandsprice@devbootcamp.com>
2015-05-20 19:50:19 -0500

    added more movies and users and reviews to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,18 @@
 genre = Genre.create(name: "Comedy")
 
-movie = genre.movies.create(title: "Ghostbusters", synopsis: "Three unemployed parapsychology professors set up shop as a unique ghost removal service.", runtime: 105)
+movie  = genre.movies.create(title: "Ghostbusters", synopsis: "Three unemployed parapsychology professors set up shop as a unique ghost removal service.", runtime: 105)
+movie2 = genre.movies.create(title: "There Will Be Blood", synopsis: "A story of family, religion, hatred, oil and madness, focusing on a turn-of-the-century prospector in the early days of the business.", runtime: 158)
+movie3 = genre.movies.create(title: "The Sound of Music", synopsis: "A woman leaves an Austrian convent to become a governess to the children of a Naval officer widower.", runtime: 174)
 
-user = User.create(name: "Bobbo", username: "Bob", password: "password")
 
-review = user.reviews.create(score: 10, body: "BEST MOVIE EVAAAAAAR!!!!! I LOVE RICK MORANIS", movie: movie)
+user  = User.create(name: "Bobbo", username: "Bob", password: "password")
+user2 = User.create(name: "Jillo", username: "Jill", password: "password")
+
+review  = user.reviews.create(score: 10, body: "BEST MOVIE EVAAAAAAR!!!!! I LOVE RICK MORANIS", movie: movie)
+review2 = user.reviews.create(score: 1, body: "This was so weird. Why all the noises?", movie: movie2)
+review3 = user.reviews.create(score: 9, body: "Such a classic! I just want to keep on singing!", movie: movie3)
+review4 = user2.reviews.create(score: 2, body: "Such a classic! I just want to keep on singing!", movie: movie3)
+
 
 review_comment = review.comments.create(body: "don't cross the streams!", user: user)
 
@@ -14,5 +22,3 @@ comment_comment = movie_comment.comments.create(body: "i agree. this movie is so
 
 comment_vote = review_comment.votes.create(direction: true, user: user)
 review_vote = review.votes.create(direction: true, user: user)
-
-
