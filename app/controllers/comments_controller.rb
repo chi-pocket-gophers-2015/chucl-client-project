0b7f52642c2389b416e98e681d0630841fd70af0
Huy Huynh <huyhuynh408@gmail.com>
2015-05-21 12:54:22 -0500

    commit for controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,9 +13,10 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @post = Comment.find_by_id(params[:comment_id])
-    @review = Review.find_by_id(params[:review_id])
-    @movie = Movie.find_by_id(params[:movie_id])
+    p movie = Movie.find_by_id(params[:movie_id])
+    p @post = Comment.find_by_id(params[:comment_id])
+    p @review = Review.find_by_id(params[:review_id])
+    p @movie = Movie.find_by_id(params[:movie_id])
     if @post
       @comment = @post.comments.create(comment_params)
     elsif @review
@@ -23,7 +24,7 @@ class CommentsController < ApplicationController
     elsif @movie
       @comment = @movie.comments.create(comment_params)
     end
-    redirect_to
+    redirect_to movie_path(movie)
   end
 
   def edit
