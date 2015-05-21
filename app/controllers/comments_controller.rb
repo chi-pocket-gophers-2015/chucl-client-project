4a378a4a519bd0b9e128598f4cf025d8f26d66cb
cjhaddad <candice.haddad@gmail.com>
2015-05-21 14:28:47 -0500

    creating a new vote for a review works. need to add user to vote

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,40 @@
+class CommentsController < ApplicationController
+  def new
+    @post = Comment.find_by_id(params[:comment_id])
+    @review = Review.find_by_id(params[:review_id])
+    @movie = Movie.find_by_id(params[:movie_id])
+    if @post
+      @comment = @post.comments.new
+    elsif @review
+      @comment = @review.comments.new
+    elsif @movie
+      @comment = @movie.comments.new
+    end
+  end
+
+  def create
+    p movie = Movie.find_by_id(params[:movie_id])
+    p @post = Comment.find_by_id(params[:comment_id])
+    p @review = Review.find_by_id(params[:review_id])
+    p @movie = Movie.find_by_id(params[:movie_id])
+    if @post
+      @comment = @post.comments.create(comment_params)
+    elsif @review
+      @comment = @review.comments.create(comment_params)
+    elsif @movie
+      @comment = @movie.comments.create(comment_params)
+    end
+    redirect_to movie_path(movie)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,15 +1,29 @@
 class VotesController < ApplicationController
 
   def new
-    @vote = Vote.new
+    @review = Review.find_by(params[:review_id])
+    @movie = Movie.find_by(params[:review_id])
+
   end
 
   def create
-    current_user.votes.create(votes_params)
+    puts params
+    puts logged_in?
+    movie = Movie.find_by_id(params[:movie_id])
+    @review = Review.find_by_id(params[:review_id])
+    vote = @review.votes.create(vote_params)
+    # vote.update_attributes(user_id: current_user.id)
+
+    redirect_to movie_path(movie)
   end
 
   def update
 
   end
 
+  private
+
+  def vote_params
+    params.require(:vote).permit(:direction)
+  end
 end
