36db773d22c35826e4e29407c19e4f19d5e2aee2
Huy Huynh <huyhuynh408@gmail.com>
2015-05-21 12:35:09 -0500

    added comments features in view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,15 @@
 class CommentsController < ApplicationController
   def new
-    @comment = Comment.find_by_id(params[:comment_id])
-    @movie = Movie.find_by_id(params[:movie_id])
+    @post = Comment.find_by_id(params[:comment_id])
     @review = Review.find_by_id(params[:review_id])
-
-
+    @movie = Movie.find_by_id(params[:movie_id])
+    if @post
+      @comment = @post.comments.new
+    elsif @review
+      @comment = @review.comments.new
+    elsif @movie
+      @comment = @movie.comments.new
+    end
   end
 
   def create
