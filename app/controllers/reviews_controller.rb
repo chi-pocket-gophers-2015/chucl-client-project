31f4f8bc82922e560aa1381d0c041a246acdac5c
lcmbl <cbarentine@gmail.com>
2015-05-21 08:54:23 -0500

    Added create action to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,15 @@ class ReviewsController < ApplicationController
   end
 
   def create
-
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.new(review_params)
+    #change line below to "current User"
+    @review.user = User.first
+    if @review.save
+      redirect_to movie_path(@movie)
+    else
+      "You Failed. Make error handling/validations"
+    end
   end
 
   def edit
@@ -14,5 +22,10 @@ class ReviewsController < ApplicationController
 
   def update
 
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:score, :body)
   end
 end
