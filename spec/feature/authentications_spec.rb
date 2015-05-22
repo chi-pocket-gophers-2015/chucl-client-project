85e97632cb7815674f1e1974409fa6822d0ae901
Huy Huynh <huyhuynh408@gmail.com>
2015-05-22 13:18:43 -0500

    added 1 user test

--- spec/feature/authentications_spec.rb
+++ spec/feature/authentications_spec.rb
@@ -7,18 +7,8 @@ feature 'User Authentication Areas' do
   let!(:review) { user.reviews.create(score: 10, body: "BEST MOVIE EVAAAAAAR!!!!! I LOVE RICK MORANIS", movie: movie, points: 0) }
 
   scenario "Goes away from review edit page when not logged in" do
-    visit genre_path(genre)
-    within('.genre-movies') do
-      click_link('The Sound of Music')
-    end
-    expect(page).to have_content('Runtime: 174')
-  end
-
-  scenario "Link for review will go to the review" do
-    visit genre_path(genre)
-    within('.genre-reviews') do
-      click_link('BEST MOVIE EVAAAAAAR!!!!! I LOVE RICK MORANIS')
-    end
-    expect(page).to have_content('Edit Review')
+    visit movie_review_path(movie, review)
+    click_link('Edit Review')
+    expect(page).to have_content("Username")
   end
 end
